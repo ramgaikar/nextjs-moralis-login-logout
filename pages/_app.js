6797b7f0d8f9c6575dd9939e3563fa04c3ddb92e
Ram Gaikar <gaikar1990@gmail.com>
2022-02-08 19:31:16 +0530

    Initial Commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
+import { MoralisProvider } from 'react-moralis'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <MoralisProvider
+      appId={process.env.NEXT_PUBLIC_MORALIS_APP_ID}
+      serverUrl={process.env.NEXT_PUBLIC_MORALIS_SERVER_URL}
+    >
+      <Component {...pageProps} />
+    </MoralisProvider>
+  )
 }
 
 export default MyApp
